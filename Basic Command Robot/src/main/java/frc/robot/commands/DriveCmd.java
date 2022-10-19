bf3759e05940cb1f426feaf0844a9165d57df902
Nave <35505431+nullphia@users.noreply.github.com>
2022-10-18 22:44:14 -0400

    Drive Subsystem and DriveCmd

---
+++ Basic Command Robot/src/main/java/frc/robot/commands/DriveCmd.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class DriveCmd extends CommandBase {
+
+  // Local instance variables
+  private final DriveSubsystem driveSubsystem; // referencing the drive subsystem class
+  private Supplier<Double> linearSpeed;
+  private Supplier<Double> turnSpeed;
+
+  // Constructor for Drive command (called when you create an instance of the command)
+  // pass in reference subsystem, and speed value
+  public DriveCmd(DriveSubsystem driveSubsystem, Supplier<Double> linearSpeed, Supplier<Double> turnSpeed) { //telling the command where to go to get the speed values
+
+
+    this.driveSubsystem = driveSubsystem; // assigning values to be used from where ever the command is called 
+    this.linearSpeed = linearSpeed; // speed variable is equal to what ever is provided in the command call
+    this.turnSpeed = turnSpeed;
+
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    driveSubsystem.drivetrain.arcadeDrive(linearSpeed.get(), turnSpeed.get());
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ Basic Command Robot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+
+public class DriveSubsystem extends SubsystemBase {
+  //define what DriveSubsystem has
+  
+  WPI_VictorSPX leftMotor1 = new WPI_VictorSPX(Constants.leftMotor1ID); //initialize leftMotor1 as a Victor SPX
+  WPI_VictorSPX leftMotor2 = new WPI_VictorSPX(Constants.leftMotor2ID); //initialize leftMotor2 as a Victor SPX
+  WPI_VictorSPX rightMotor1 = new WPI_VictorSPX(Constants.rightMotor1ID); //initialize rightMotor1 as a Victor SPX
+  WPI_VictorSPX rightMotor2 = new WPI_VictorSPX(Constants.rightMotor2ID); //initialize rightMotor2 as a Victor SPX
+
+  
+  MotorControllerGroup right = new MotorControllerGroup(rightMotor1, rightMotor2); //right motor 
+  MotorControllerGroup left = new MotorControllerGroup(leftMotor1, leftMotor2);
+
+  public DifferentialDrive drivetrain = new DifferentialDrive(left, right);
+  
+
+
+  /** Creates a new Drive Subsystem. */
+  public DriveSubsystem() { 
+      // initialize when driveSubsystem is created
+      rightMotor1.setInverted(true); 
+      rightMotor2.setInverted(true);
+    }
+
+  
+
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    //SmartDashboard.putData("Left Drive Train Raw Speed", this.);
+
+
+  }
+
+
+
+}
